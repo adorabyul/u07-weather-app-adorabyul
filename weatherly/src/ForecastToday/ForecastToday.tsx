1234579357e54b816a354487df7839af53029045
William Carlström <vbw123@live.se>
2023-04-26 16:07:21 +0200

    removed unused import

--- weatherly/src/ForecastToday/ForecastToday.tsx
+++ weatherly/src/ForecastToday/ForecastToday.tsx
@@ -1,5 +1,5 @@
 
-import Flicking, { FreeControl } from "@egjs/react-flicking";
+import Flicking from "@egjs/react-flicking";
 import "@egjs/react-flicking/dist/flicking.css";
 import './ForecastToday.scss';
 
@@ -8,7 +8,7 @@ export default function ForecastToday(props: any) {
     const renderHours = () => {
         const hourList = [];
         for(let i = 0; i < props.forecast.length; i++)
-        {
+        {   
             let temp = props.forecast[i].temp_c;
             let hour = props.forecast[i].time.slice(-5, -3)
             let icon = "https:" + props.forecast[i].condition.icon;
